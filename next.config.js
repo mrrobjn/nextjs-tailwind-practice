1561456a97b4809d5955a6760fab6e0210260050
Võ Trường Dũng <87188466+mrrobjn@users.noreply.github.com>
2024-01-20 23:32:36 +0700

    fix cors

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: process.env.BASE_URL },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE" },
+        ],
+      },
+    ];
+  },
   env: {
     BASE_URL: process.env.BASE_URL,
     MONGODB_URL: process.env.MONGODB_URL,
   },
 };
+
+module.exports = nextConfig;
